fa082d4a88a9f82c1b61890b24442999fe5ea446
ChabaJosa <chabagjg@gmail.com>
2021-10-11 16:31:31 -0400

    Added background image to details

--- src/screens/details.js
+++ src/screens/details.js
@@ -6,10 +6,15 @@ import {
   SafeAreaView,
   ScrollView,
   ActivityIndicator,
+  Dimensions,
+  ImageBackground,
 } from "react-native";
 import { Context } from "../Context/AppProvider";
 import { Image } from "react-native-elements";
+import { LinearGradient } from "expo-linear-gradient";
 import { useIsFocused } from "@react-navigation/native";
+//
+const { height, width } = Dimensions.get("screen");
 //
 export default function Details({ route }) {
   //
@@ -36,99 +41,112 @@ export default function Details({ route }) {
     //
     return (
       <SafeAreaView style={styles.container}>
-        <ScrollView contentContainerStyle={styles.containerStyle}>
-          <View style={[styles.flexOneRow, { justifyContent: "center" }]}>
-            <Image
-              source={{ uri: state.movieData[index].Poster }}
-              style={styles.avatar}
-            />
-          </View>
-          <View style={[styles.flexOneRow, { justifyContent: "center" }]}>
-            <Text style={styles.title}>{state.movieData[index].Title}</Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Plot:</Text>
-            <Text style={[styles.textIconic, { textAlign: "justify" }]}>
-              {state.movieData[index].Plot}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Genre:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Genre}{" "}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Director:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Director}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Writer(s):</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Writer}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Actors:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Actors}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Box Office:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].BoxOffice}{" "}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Released:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Released}{" "}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Awards:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Awards}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Production House:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Production}{" "}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Rated:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Rated}{" "}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Runtime:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Runtime}{" "}
-            </Text>
-          </View>
-          <View style={styles.flexOneCol}>
-            <Text style={styles.dataLabel}>Language:</Text>
-            <Text style={styles.textIconic}>
-              {state.movieData[index].Language}{" "}
-            </Text>
-          </View>
-          {state.movieData[index].Ratings[1] !== undefined ? (
+        <ImageBackground
+          source={{ uri: state.movieData[index].Poster }}
+          imageStyle={{ borderRadius: 16 }}
+          resizeMode="cover"
+          style={{ width, height }}
+        >
+          <LinearGradient
+            colors={["transparent", "black"]}
+            start={{ x: 0.9, y: 0.5 }}
+            end={{ x: 0.1, y: 0.5 }}
+            style={{ width, height, position: "absolute", bottom: 0 }}
+          />
+          <ScrollView contentContainerStyle={styles.containerStyle}>
+            <View style={[styles.flexOneRow, { justifyContent: "center" }]}>
+              <Image
+                source={{ uri: state.movieData[index].Poster }}
+                style={styles.avatar}
+              />
+            </View>
+            <View style={[styles.flexOneRow, { justifyContent: "center" }]}>
+              <Text style={styles.title}>{state.movieData[index].Title}</Text>
+            </View>
             <View style={styles.flexOneCol}>
-              <Text style={styles.dataLabel}>
-                {`${state.movieData[index].Ratings[1].Source}`} Score:
-              </Text>
-              <Text style={styles.textIconic}>
-                {state.movieData[index].Ratings[1].Value}
+              <Text style={styles.dataLabel}>Plot:</Text>
+              <Text style={[styles.textIconic, { textAlign: "justify" }]}>
+                {state.movieData[index].Plot}
               </Text>
             </View>
-          ) : null}
-        </ScrollView>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Genre:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Genre}{" "}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Director:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Director}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Writer(s):</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Writer}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Actors:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Actors}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Box Office:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].BoxOffice}{" "}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Released:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Released}{" "}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Awards:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Awards}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Production House:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Production}{" "}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Rated:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Rated}{" "}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Runtime:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Runtime}{" "}
+              </Text>
+            </View>
+            <View style={styles.flexOneCol}>
+              <Text style={styles.dataLabel}>Language:</Text>
+              <Text style={styles.textIconic}>
+                {state.movieData[index].Language}{" "}
+              </Text>
+            </View>
+            {state.movieData[index].Ratings[1] !== undefined ? (
+              <View style={styles.flexOneCol}>
+                <Text style={styles.dataLabel}>
+                  {`${state.movieData[index].Ratings[1].Source}`} Score:
+                </Text>
+                <Text style={styles.textIconic}>
+                  {state.movieData[index].Ratings[1].Value}
+                </Text>
+              </View>
+            ) : null}
+          </ScrollView>
+        </ImageBackground>
       </SafeAreaView>
     );
   } else {
@@ -148,8 +166,9 @@ const styles = StyleSheet.create({
   },
   containerStyle: {
     // flex: 1,
+    // zIndex: 1,
+    // backgroundColor: "black",
     minHeight: 900,
-    backgroundColor: "black",
     justifyContent: "space-around",
     alignItems: "center",
     margin: 8,
